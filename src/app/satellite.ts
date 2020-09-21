75398e85a8cfe1da027c22b1d80be1111c491998
Colin Park <colinpark89@gmail.com>
2020-09-20 22:04:47 -0500

    Finished Assignment 6

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,12 +4,22 @@ export class Satellite {
   type: string;
   operational: boolean;
   launchDate: string;
+  showWarning: boolean
 
-  constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
     this.name = name;
     this.type = type;
     this.launchDate = launchDate;
     this.operational = operational;
     this.orbitType = orbitType;
+    this.shouldShowWarning();
+  }
+
+  shouldShowWarning = function() {
+    if (this.type.toLowerCase() === "space debris") {
+     this.showWarning = true;
+    } else {
+      this.showWarning = false
+    }
   }
 }
